b0b6941eda3d91bc7c690c968818b82c4ec26486
Oleg Selin <oleg.selin@gmail.com>
2017-12-17 18:35:06 +0300

    Initial commit

---
+++ helloworld/helloworld.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import time
+
+def main():
+    print('hello world!')
+    time.sleep(5)
+    print('goodbye world!')
+    time.sleep(3)
+
+if __name__ == '__main__':
+   main()
+
